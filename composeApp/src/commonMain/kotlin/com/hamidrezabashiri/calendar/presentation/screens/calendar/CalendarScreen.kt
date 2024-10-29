1ee46a0d7ff0d38f965311a3bcce5a1db99ffd02
HamidReza Bashiri <hamidrezabashiri@proton.me>
2024-10-29 18:19:06 +0330

    CalendarScreen minor ui change

--- composeApp/src/commonMain/kotlin/com/hamidrezabashiri/calendar/presentation/screens/calendar/CalendarScreen.kt
+++ composeApp/src/commonMain/kotlin/com/hamidrezabashiri/calendar/presentation/screens/calendar/CalendarScreen.kt
@@ -1,5 +1,8 @@
 package com.hamidrezabashiri.calendar.presentation.screens.calendar
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,17 +10,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.hamidrezabashiri.calendar.di.ViewModelProvider
 import com.hamidrezabashiri.calendar.domain.model.CalendarEventModel
 import com.hamidrezabashiri.calendar.presentation.screens.base.BaseScreen
+import io.wojciechosak.calendar.config.DayState
+import io.wojciechosak.calendar.config.rememberCalendarState
+import io.wojciechosak.calendar.view.CalendarView
+import io.wojciechosak.calendar.view.HorizontalCalendarView
+import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
 
 @Composable
 fun CalendarScreen() {
@@ -57,6 +72,7 @@ fun CalendarScreen() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun CalendarContent(
     state: CalendarContract.State,
@@ -73,6 +89,25 @@ private fun CalendarContent(
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
+            val currentDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
+
+            // Adjust to the first day of the current month
+
+            HorizontalCalendarView(
+                startDate = currentDate,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                calendarView = { monthOffset ->
+                    CalendarView(
+                        day = { dayState ->
+                            CustomCalendarDay(state = dayState)
+                        },
+                        config = rememberCalendarState(
+                            startDate = currentDate,
+                            monthOffset = monthOffset
+                        )
+                    )
+                }
+            )
             // Calendar date picker
 //            CalendarDatePicker(
 //                selectedDate = state.selectedDate,
@@ -155,5 +190,25 @@ private fun EventItem(
                 Text(text = description)
             }
         }
+    }
+}
+@Composable
+fun CustomCalendarDay(state: DayState) {
+    val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+    val isToday = state.date == today
+    val backgroundColor = if (isToday) Color.Yellow else Color.Transparent
+    val textColor = if (isToday) Color.Green else Color.Black
+    val borderColor = if (isToday) Color.Green else Color.Transparent
+    Box(
+        modifier = Modifier.padding(8.dp)
+            .size(40.dp)
+            .border( BorderStroke(1.dp,borderColor), CircleShape  ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = state.date.dayOfMonth.toString(),
+            color = textColor,
+            style = MaterialTheme.typography.body1
+        )
     }
 }
